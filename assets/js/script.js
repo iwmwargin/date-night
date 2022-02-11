60c4af28f828ff1fd73d7f83a2c973346cb249b7
iwmwargin <95541348+iwmwargin@users.noreply.github.com>
2022-02-10 20:33:45 -0500

    added code for places api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,52 @@
-// Define the settings for the API call
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=dog+friendly+locations&sort_by=distance&location=${window.searchText}`,
-  "method": "GET",
-  "headers": {
-    "authorization": "Bearer ZMRol_fEx3vgsmM6XbAhsFNmcSt_FcqkeFnf6nW8xql3-XjYW4uqQMFOm5S1ZKN7BJ-GAk9Sx3fLNlNeoB_hI1UeUvAno8gCztKAU1_2nYbiHnBU1fVTj5xC71AEYnYx",
-    "cache-control": "no-cache",
-    "postman-token": "3f23d8c3-ce48-a224-50c0-14b9094948fc"
-  }
-}
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -33.866, lng: 151.196 },
+    zoom: 15,
+  });
+  const request = {
+    placeId: "ChIJN1t_tDeuEmsRUsoyG83frY4",
+    fields: ["name", "formatted_address", "place_id", "geometry"],
+  };
+  const infowindow = new google.maps.InfoWindow();
+  const service = new google.maps.places.PlacesService(map);
 
+  service.getDetails(request, (place, status) => {
+    if (
+      status === google.maps.places.PlacesServiceStatus.OK &&
+      place &&
+      place.geometry &&
+      place.geometry.location
+    ) {
+      const marker = new google.maps.Marker({
+        map,
+        position: place.geometry.location,
+      });
+
+      google.maps.event.addListener(marker, "click", () => {
+        const content = document.createElement("div");
+        const nameElement = document.createElement("h2");
+
+        nameElement.textContent = place.name;
+        content.appendChild(nameElement);
+
+        const placeIdElement = document.createElement("p");
+
+        placeIdElement.textContent = place.place_id;
+        content.appendChild(placeIdElement);
+
+        const placeAddressElement = document.createElement("p");
+
+        placeAddressElement.textContent = place.formatted_address;
+        content.appendChild(placeAddressElement);
+        infowindow.setContent(content);
+        infowindow.open(map, marker);
+      });
+    }
+  });
+}
 //Selectors
 var searchButton = document.querySelector("#search-submit")
 var searchInput = document.querySelector("#search-input") 
