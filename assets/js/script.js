ad10ad9850d8731bef1a538c9407cc7f884a1aaf
Leigh Pfeiffer <leigh.pfeiffer@gmail.com>
2022-02-11 17:17:41 -0500

    search history

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,40 @@
-// This example requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -33.866, lng: 151.196 },
-    zoom: 15,
-  });
-  const request = {
-    placeId: "ChIJN1t_tDeuEmsRUsoyG83frY4",
-    fields: ["name", "formatted_address", "place_id", "geometry"],
-  };
-  const infowindow = new google.maps.InfoWindow();
-  const service = new google.maps.places.PlacesService(map);
+// setting for API Call
 
-  service.getDetails(request, (place, status) => {
-    if (
-      status === google.maps.places.PlacesServiceStatus.OK &&
-      place &&
-      place.geometry &&
-      place.geometry.location
-    ) {
-      const marker = new google.maps.Marker({
-        map,
-        position: place.geometry.location,
-      });
 
-      google.maps.event.addListener(marker, "click", () => {
-        const content = document.createElement("div");
-        const nameElement = document.createElement("h2");
 
-        nameElement.textContent = place.name;
-        content.appendChild(nameElement);
-
-        const placeIdElement = document.createElement("p");
-
-        placeIdElement.textContent = place.place_id;
-        content.appendChild(placeIdElement);
-
-        const placeAddressElement = document.createElement("p");
-
-        placeAddressElement.textContent = place.formatted_address;
-        content.appendChild(placeAddressElement);
-        infowindow.setContent(content);
-        infowindow.open(map, marker);
-      });
-    }
-  });
-}
 //Selectors
 var searchButton = document.querySelector("#search-submit")
 var searchInput = document.querySelector("#search-input") 
+var searchResults = document.querySelector("#search-results")
+var historyContainerE1 = document.querySelector("#search-data-list")
 
-//the search button
+
+//search history
+var createHistoryDropdown = function(){
+  historyContainerE1.innerHTML = "";
+  historyContainerEl.innerHTML = "";
+    var searchHistoryArr = JSON.parse(localStorage.getItem("searchHistoryArr"));
+    if (searchHistoryArr != null){
+        searchHistoryArr = searchHistoryArr.sort();
+        console.log(searchHistoryArr);
+        for (var i=0;i<searchHistoryArr.length;i++){
+          //console.log(searchHistoryArr[i]);
+          var historyListItem = document.createElement("option");
+          historyListItem.value = searchHistoryArr[i];
+          historyListItem.text = searchHistoryArr[i];
+          historyContainerEl.appendChild(historyListItem);
+}
+}
+}
+ 
+//store search hx
+var storeSearchHistory = function(searchValue){
+  var cleanedSearchValue = searchValue.toLowerCase().trim();
+  var searchHistoryArr = JSON.parse(localStorage.getItem("searchHistoryArr"))
+}
+
+//search button
 searchButton.addEventListener("click", function(event) {
-  
+
 })
 
