4050dfe31aa19b074e884a9d85fba5435d87b811
Leigh Pfeiffer <leigh.pfeiffer@gmail.com>
2022-02-14 15:33:10 -0500

    added new API, going to switch computers

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,16 +15,17 @@ var historyContainerEl = document.querySelector("#search-data-list")
 var wineList = document.querySelector("#wine-list")
 var pairingText = document.querySelector("#pairing-text")
 var recentHistory = document.querySelector("#recent-history")
+var recipeList = document.querySelector("#recipe-list")
 
 //render search history
-var renderSearchHistory = function(){
+var renderSearchHistory = function () {
   recentHistory.innerHTML = ""
-  for (var i = 0 ; i < searchHistory.length; i++){
+  for (var i = 0; i < searchHistory.length; i++) {
     var button = document.createElement("button")
     button.setAttribute("class", "button")
-    button.addEventListener("click", function(event){
+    button.addEventListener("click", function (event) {
       console.log(event.target.innerHTML)
-     renderSearch(event.target.innerHTML)
+      renderSearch(event.target.innerHTML)
     })
     button.innerHTML = searchHistory[i]
     recentHistory.appendChild(button)
@@ -34,14 +35,14 @@ var renderSearchHistory = function(){
 
 //store search history
 var storeSearchHistory = function (search) {
-  if(searchHistory.length >= 5){
+  if (searchHistory.length >= 5) {
     searchHistory.pop()
   }
   searchHistory.unshift(search)
   console.log(searchHistory)
   localStorage.setItem("Searchhistory", JSON.stringify(searchHistory))
   renderSearchHistory()
-  
+
 }
 
 // rendering search
@@ -75,6 +76,31 @@ var renderSearch = function (search) {
         console.log(data);
       })
     })
+  fetch("https://tasty.p.rapidapi.com/recipes/list?from=0&size=5&q=" + search, {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "tasty.p.rapidapi.com",
+      "x-rapidapi-key": "5a14697b02msh42244a1beada018p100b3ajsn699e260e7f11"
+    }
+  })
+
+    .then(response => {
+      console.log(response);
+      response.json().then((data) => {
+        console.log(data)
+        recipeList.innerHTML = ""
+        try {
+          for (var i = 0; i < results.length; i++) {
+            var listItem = document.createElement("li")
+            listItem.innerHTML = recipeList[i]
+            recipeList.appendChild(listItem)
+          }
+        } catch (error) {
+          recipeList.innerHTML = "No Recipies Found"
+        }
+      })
+    })
+
 }
 
 //search button
