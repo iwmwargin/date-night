f2fd0a680ebb467a25c36109ef04c5ac18902c5f
Leigh Pfeiffer <leigh.pfeiffer@gmail.com>
2022-02-13 20:20:51 -0500

    saves sreach to local storage and render to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,71 +1,82 @@
 // setting for API Call
 
-
+//global varibles 
+if (localStorage.getItem("Searchhistory")) {
+  var searchHistory = JSON.parse(localStorage.getItem("Searchhistory"))
+} else {
+  var searchHistory = []
+}
 
 //Selectors
 var searchButton = document.querySelector("#search-submit")
-var searchInput = document.querySelector("#search-input") 
+var searchInput = document.querySelector("#search-input")
 var searchResults = document.querySelector("#search-results")
 var historyContainerEl = document.querySelector("#search-data-list")
 var wineList = document.querySelector("#wine-list")
 var pairingText = document.querySelector("#pairing-text")
+var recentHistory = document.querySelector("#recent-history")
 
-//search history
-var createHistoryDropdown = function(){
-  historyContainerEl.innerHTML = "";
-  historyContainerEl.innerHTML = "";
-    var searchHistoryArr = JSON.parse(localStorage.getItem("searchHistoryArr"));
-    if (searchHistoryArr != null){
-        searchHistoryArr = searchHistoryArr.sort();
-        console.log(searchHistoryArr);
-        for (var i=0;i<searchHistoryArr.length;i++){
-          //console.log(searchHistoryArr[i]);
-          var historyListItem = document.createElement("option");
-          historyListItem.value = searchHistoryArr[i];
-          historyListItem.text = searchHistoryArr[i];
-          historyContainerEl.appendChild(historyListItem);
+//render search history
+var renderSearchHistory = function(){
+  recentHistory.innerHTML = ""
+  for (var i = 0 ; i < searchHistory.length; i++){
+    var button = document.createElement("button")
+    button.setAttribute("class", "button")
+    button.innerHTML = searchHistory[i]
+    recentHistory.appendChild(button)
+    console.log(searchHistory[i])
+  }
 }
+
+//store search history
+var storeSearchHistory = function () {
+  if(searchHistory.length >= 5){
+    searchHistory.pop()
+  }
+  searchHistory.unshift(searchInput.value)
+  console.log(searchHistory)
+  localStorage.setItem("Searchhistory", JSON.stringify(searchHistory))
+  renderSearchHistory()
+  
 }
-}
- 
-//store search hx
-var storeSearchHistory = function(searchValue){
-  var cleanedSearchValue = searchValue.toLowerCase().trim();
-  var searchHistoryArr = JSON.parse(localStorage.getItem("searchHistoryArr"))
+
+// rendering search
+var renderSearch = function () {
+  fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/wine/pairing?food=" + searchInput.value + "&maxPrice=50", {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+      "x-rapidapi-key": "eff664db17mshe95b0e4695a6a7cp1b915ejsnbf16eceafe4c"
+    }
+  })
+
+    .then((response) => {
+      console.log(response);
+      response.json().then((data) => {
+        wines = data.pairedWines
+        pairingText.innerHTML = data.pairingText
+        wineList.innerHTML = ""
+        try {
+          for (var i = 0; i < wines.length; i++) {
+            var listItem = document.createElement("li")
+            listItem.setAttribute("class", "wine-item")
+            listItem.innerHTML = wines[i]
+            wineList.appendChild(listItem)
+          }
+        } catch (error) {
+          pairingText.innerHTML = "Result not found. Please try again."
+
+        }
+
+        console.log(data);
+      })
+    })
 }
 
 //search button
-searchButton.addEventListener("click", function(event) {
-  fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/wine/pairing?food=" + searchInput.value + "&maxPrice=50", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-		"x-rapidapi-key": "eff664db17mshe95b0e4695a6a7cp1b915ejsnbf16eceafe4c"
-	}
-})
-
-.then((response) => {
-	console.log(response);
-  response.json().then((data) => {
-    wines = data.pairedWines
-    pairingText.innerHTML = data.pairingText
-    wineList.innerHTML = ""
-    try {
-      for (var i = 0; i < wines.length; i++){
-          var listItem = document.createElement("li")
-          listItem.setAttribute("class", "wine-item")
-          listItem.innerHTML = wines[i]
-          wineList.appendChild(listItem)
-      }
-    } catch (error) {
-      pairingText.innerHTML = "Result not found. Please try again."
-      
-    }
-    
-    console.log(data);
-  })
-})
-
+searchButton.addEventListener("click", function (event) {
+  storeSearchHistory()
+  renderSearch()
 });
 
-
+renderSearchHistory()
