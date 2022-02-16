578e84d62c56d51e63c4228dae06aec32b93310a
Leigh Pfeiffer <leigh.pfeiffer@gmail.com>
2022-02-15 19:45:29 -0500

    JAvascript changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,33 +15,34 @@ var historyContainerEl = document.querySelector("#search-data-list")
 var wineList = document.querySelector("#wine-list")
 var pairingText = document.querySelector("#pairing-text")
 var recentHistory = document.querySelector("#recent-history")
+var recipeResults = document.querySelector("#recipe-results")
 
 //render search history
-var renderSearchHistory = function(){
+var renderSearchHistory = function () {
   recentHistory.innerHTML = ""
-  for (var i = 0 ; i < searchHistory.length; i++){
+  for (var i = 0; i < searchHistory.length; i++) {
     var button = document.createElement("button")
     button.setAttribute("class", "button")
-    button.addEventListener("click", function(event){
-      console.log(event.target.innerHTML)
-     renderSearch(event.target.innerHTML)
+    button.addEventListener("click", function (event) {
+      //console.log(event.target.innerHTML)
+      renderSearch(event.target.innerHTML)
     })
     button.innerHTML = searchHistory[i]
     recentHistory.appendChild(button)
-    console.log(searchHistory[i])
+    //console.log(searchHistory[i])
   }
 }
 
 //store search history
 var storeSearchHistory = function (search) {
-  if(searchHistory.length >= 5){
+  if (searchHistory.length >= 5) {
     searchHistory.pop()
   }
   searchHistory.unshift(search)
-  console.log(searchHistory)
+  //console.log(searchHistory)
   localStorage.setItem("Searchhistory", JSON.stringify(searchHistory))
   renderSearchHistory()
-  
+
 }
 
 // rendering search
@@ -53,13 +54,15 @@ var renderSearch = function (search) {
       "x-rapidapi-key": "eff664db17mshe95b0e4695a6a7cp1b915ejsnbf16eceafe4c"
     }
   })
-
-    .then((response) => {
-      console.log(response);
-      response.json().then((data) => {
-        wines = data.pairedWines
-        pairingText.innerHTML = data.pairingText
-        wineList.innerHTML = ""
+  
+  .then((response) => {
+    //console.log(response);
+    response.json().then((data) => {
+      console.log(data)
+      wines = data.pairedWines
+      pairingText.innerHTML = data.pairingText
+      wineList.innerHTML = ""
+      recipeResults.innerHTML = ""
         try {
           for (var i = 0; i < wines.length; i++) {
             var listItem = document.createElement("li")
@@ -72,9 +75,51 @@ var renderSearch = function (search) {
 
         }
 
-        console.log(data);
+       
       })
     })
+  fetch("https://tasty.p.rapidapi.com/recipes/list?from=0&size=5&q=" + search, {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "tasty.p.rapidapi.com",
+      "x-rapidapi-key": "5a14697b02msh42244a1beada018p100b3ajsn699e260e7f11"
+    }
+  })
+
+    .then(response => {
+      //console.log(response);
+      response.json().then((data) => {
+        console.log(data.results)
+        // try {
+        for (var i = 0; i < data.results.length; i++) {
+          if (data.results[i].instructions) {
+            var recipe = document.createElement("div")
+            var recipeName = document.createElement("div")
+            recipe.setAttribute("class", "recipe")
+            recipeName.setAttribute("class", "recipe-name")
+            recipeName.innerHTML = data.results[i].name
+            recipe.appendChild(recipeName)
+            var instructions = document.createElement("ul")
+            instructions.setAttribute("class", "instructions")
+            for (var r = 0; r < data.results[i].instructions.length; r++) {
+              var step = document.createElement("li")
+              step.setAttribute("class", "step")
+              step.innerHTML = data.results[i].instructions[r].display_text
+              instructions.appendChild(step)
+            }
+            recipe.appendChild(instructions)
+          }
+
+          recipeResults.appendChild(recipe)
+        }
+
+        //} 
+        // catch (error) {
+        //   recipeResults.innerHTML = "No Recipies Found"
+        // }
+      })
+    })
+
 }
 
 //search button
